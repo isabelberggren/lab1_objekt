c2bdd07fd412d5648c90e151bf3d85ef5802c1c6
isabelberggren <berggren.isabel@gmail.com>
2025-01-28 11:01:10 +0100

    Uppgift 1

--- Volvo240.java
+++ Volvo240.java
@@ -1,13 +1,9 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Cars{
 
-    public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    private final static double trimFactor = 1.25;
+
     
     public Volvo240(){
         nrDoors = 4;
@@ -17,34 +13,9 @@ public class Volvo240{
         stopEngine();
     }
     
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
     
-    public double speedFactor(){
+    private double speedFactor(){
         return enginePower * 0.01 * trimFactor;
     }
 
