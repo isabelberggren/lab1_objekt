44be3c42a8694cf307b6655805c655082398fb25
AnnaHiljemark <annahiljemark05@gmail.com>
2025-02-03 16:32:16 +0100

    Vehicle istället för cars

--- Scania.java
+++ Scania.java
@@ -1,4 +1,15 @@
+import java.awt.*;
+
 public class Scania extends Vehicle{
+
+    public Scania(){
+        nrDoors = 2;
+        color = Color.orange;
+        enginePower = 1500;
+        modelName = "Scania";
+        ScaniaAngle = 0;
+        stopEngine();
+        }
 
 
 
