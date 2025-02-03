4adeded1e0164302b61c08817ee1dec96300e1af
elliotn1337 <elliotn@student.chalmers.se>
2025-02-03 17:25:35 +0100

    Uppgift 1: Extensibilitet utan test

--- Scania.java
+++ Scania.java
@@ -2,28 +2,45 @@ import java.awt.*;
 
 public class Scania extends Vehicle{
 
-    private int scaniaAngle;
+    private int angle;
 
     public Scania(){
         nrDoors = 2;
         color = Color.orange;
         enginePower = 1500;
         modelName = "Scania";
-        scaniaAngle = 0;
+        angle = 0;
         stopEngine();
         }
 
 
-        public int getAngle(){return scaniaAngle;}
+        public int getAngle(){return angle;}
 
-        public void setAngle(int angle){
-            if (angle >= 0 && 70 >=angle){
-                this.scaniaAngle = angle;
+        public void setAngle(int angle) {
+            double speed = getCurrentSpeed();
+            if (speed == 0) {
+                if (angle >= 0 && 70 >= angle) {
+                    this.angle = angle;
+                } else {
+                    throw new IllegalArgumentException("Angle should be between 0 and 70 degrees");
+                }
+            } else {
+                throw new RuntimeException("Speed must be 0 to change angle");
             }
-            else{throw new IllegalArgumentException("Angle should be between 0 and 70 degrees");}
-    }
+        }
+        @Override
+        public void startEngine(){
+            int placeholder = getAngle();
+
+            if (placeholder == 0) {currentSpeed = 0.1;}
+
+            else{throw new IllegalArgumentException("Angle must be 0 to start the engine");}
+        }
 
 
-
-'
 }
+
+
+
+
+
