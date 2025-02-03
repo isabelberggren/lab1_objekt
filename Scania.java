b10a4860034be72ce3d8ab51fed20f4ff95cc4ec
elliotn1337 <elliotn@student.chalmers.se>
2025-02-03 16:55:37 +0100

    set/getAngle

--- Scania.java
+++ Scania.java
@@ -2,21 +2,28 @@ import java.awt.*;
 
 public class Scania extends Vehicle{
 
+    private int scaniaAngle;
+
     public Scania(){
         nrDoors = 2;
         color = Color.orange;
         enginePower = 1500;
         modelName = "Scania";
-        int ScaniaAngle = 0;
+        scaniaAngle = 0;
         stopEngine();
         }
 
 
-        public void getAngle{
+        public int getAngle(){return scaniaAngle;}
 
+        public void setAngle(int angle){
+            if (angle >= 0 && 70 >=angle){
+                this.scaniaAngle = angle;
+            }
+            else{throw new IllegalArgumentException("Angle should be between 0 and 70 degrees");}
     }
 
 
 
-
+'
 }
